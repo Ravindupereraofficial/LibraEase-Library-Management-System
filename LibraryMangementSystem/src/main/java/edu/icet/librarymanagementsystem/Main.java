5d894ec8936e2823535c850e2608b35b1636d46b
Ravindu Perera <ravindurandika2004@gmail.com>
2025-01-19 21:46:52 +0530

    db Connection sngleton

--- LibraryMangementSystem/src/main/java/edu/icet/librarymanagementsystem/Main.java
+++ LibraryMangementSystem/src/main/java/edu/icet/librarymanagementsystem/Main.java
@@ -1,8 +1,15 @@
 package edu.icet.librarymanagementsystem;
 
+import edu.icet.librarymanagementsystem.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Starter.main(args);
+        Connection connection= DBConnection.getInstance().getConnection();
+        System.out.println(connection);
     }
 
 }
